b69ead807c9218aa242e85da9331b58683090fef
CHOOSLA <cnckddn0146@gmail.com>
2021-09-26 23:35:01 +0900

    Midterm

--- main.py
+++ main.py
@@ -1,39 +1,97 @@
 import random
+import threading
 from tkinter import *
+from tkinter import ttk
+
 from PIL import ImageTk, Image
-from piece import Piece
+
+import astar
+from astar import *
 
 
-def updatePuzzle():
-    count = 0
-    for i in range(3):
-        for j in range(3):
-            piece[count].grid(column=j, row=i)
-            count += 1
+def updatePuzzle(board):
+    for i in range(9):
+        piece[i].grid(column=board[i] % 3, row=int(board[i] / 3))
     win.update()
 
 
 def randomize():
-    random.shuffle(piece)
-    updatePuzzle()
+    random.shuffle(puzzle)
+    updatePuzzle(puzzle)
 
+
+def search():
+    searchButton['state'] = DISABLED
+    loopChosen['state'] = DISABLED
+    randomizeButton['state'] = DISABLED
+    # open 리스트는 우선순위 큐로 생성한다.
+    open_queue = queue.PriorityQueue()
+
+    open_queue.put(State(puzzle, goal))
+
+    closed_queue = []
+
+    count = 0;
+    while not open_queue.empty() and (count < int(loopChosen.get()) or int(loopChosen.get()) == -1):
+        #  디버깅을 위한 코드
+        #  print("START OF OPENQ")
+        #  for elem in open_queue.queue:
+        #        print(elem)
+        #  print("END OF OPENQ")
+
+        current = open_queue.get()
+        count += 1
+        print(count)
+
+        updatePuzzle(current.board)
+        if current.board == goal:
+            updatePuzzle(current.board)
+            print(current)
+            print("탐색 성공")
+            print("open queue 길이=", open_queue.qsize())
+            print("closed queue 길이=", len(closed_queue))
+            updatePuzzle()
+            break
+        moves = current.moves + 1
+        for state in current.expand(moves):
+            if state not in closed_queue and state not in open_queue.queue:
+                open_queue.put(state)
+        closed_queue.append(current)
+    else:
+        print('탐색 실패')
+
+    randomizeButton['state'] = NORMAL
+    searchButton['state'] = NORMAL
+    loopChosen['state'] = NORMAL
+
+
+def threadSearch():
+    t = threading.Thread(target=search)
+    t.start()
+
+
+# 초기 상태
+puzzle = [0, 1, 2,
+          3, 4, 5,
+          6, 7, 8]
+
+# 목표 상태
+goal = [0, 1, 2,
+        3, 4, 5,
+        6, 7, 8]
 
 win = Tk()
 win.title("8 PUZZLE SOLVER")
-win.geometry("1200x800")
+win.geometry("400x800")
 ####win.resizable(False, False)
 
 leftPanel = LabelFrame(win, width=400)
-leftPanel.configure(background='black')
 leftPanel.pack(fill='y', side='left')
 
-rightPanel = LabelFrame(win, width=400)
-rightPanel.configure(background='black')
-rightPanel.pack(fill='both', expand=1, side='right')
 
-puzzle = LabelFrame(leftPanel, width=600, height=600)
-puzzle.configure(background='red')
-puzzle.grid(column=0, row=0, padx=50, pady=50)
+puzzlePanel = LabelFrame(leftPanel, width=600, height=600)
+puzzlePanel.configure(background='red')
+puzzlePanel.grid(column=0, row=0, padx=50, pady=50)
 
 piece = []
 img = []
@@ -46,7 +104,7 @@ for i in range(3):
 
         img.append(tmp)
 
-        lbl = Label(puzzle, background='red', image=img[count], width=100)
+        lbl = Label(puzzlePanel, background='red', text=count, image=img[count], width=100)
         lbl.grid(column=j, row=i)
         piece.append(lbl)
         count += 1
@@ -57,7 +115,25 @@ buttonPanel = LabelFrame(leftPanel)
 buttonPanel.configure(background='white')
 buttonPanel.grid(column=0, row=2, padx=50, pady=50)
 
-Button(buttonPanel, text="Randomize", width=30, height=5, command=randomize).grid(column=0, row=0, padx=10, pady=10)
-Button(buttonPanel, text="Search", width=30, height=5).grid(column=0, row=5, padx=10, pady=10)
+loop = 10000
+
+loopChosen = ttk.Combobox(buttonPanel, width=12, textvariable=loop)
+loopChosen.grid(column=0, row=0, padx=10, pady=10)
+loopChosen['values'] = (-1, 10000, 20000, 50000, 100000)
+loopChosen.current(1)
+
+randomizeButton = Button(buttonPanel, text="Randomize", width=30, height=5, command=randomize)
+randomizeButton.grid(column=0, row=1, padx=10, pady=10)
+searchButton = Button(buttonPanel, text="Search", width=30, height=5, command=threadSearch)
+searchButton.grid(column=0, row=2, padx=10, pady=10)
+
+# 목표 상태
+goal = [0, 1, 2,
+        3, 4, 5,
+        6, 7, 8]
+
+open_queue = queue.PriorityQueue()
+closed_queue = []
+moves = 0
 
 win.mainloop()
